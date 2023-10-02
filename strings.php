363009286904e4512bb12daa34fe96ff6733a7c5
SAJAL KHAWAS <khawassajal756@gmail.com>
2023-10-03 01:20:22 +0530

    Strings practice

---
+++ strings.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $str = "Hello world";
+    // echo addcslashes($str, 'w');
+    // echo "<br>";
+    // echo addcslashes($str, 'a..z');
+    
+    // $str="That's it";
+    // echo addslashes($str);
+    
+    // echo bin2hex($str);
+    
+    // echo chop($str, 't');
+    
+    // echo chr(52);
+    
+    // echo chunk_split($str, 2, "...");
+    
+    // echo convert_uuencode($str);
+    // echo "<br>";
+    // echo convert_uudecode(convert_uuencode($str));
+    
+    // print_r(count_chars($str));
+    // print_r(count_chars($str, 1));
+    // print_r(count_chars($str, 4));
+    // $charsUsed = count_chars($str, 1);
+    // foreach ($charsUsed as $key => $value) {
+    //     echo chr($key) . ": " . $value . "<br>";
+    // }
+    
+    // $str = 'one,two,three,four';
+    
+    // // zero limit
+    // print_r(explode(',', $str, 0));
+    // echo "<br>";
+    
+    // // positive limit
+    // print_r(explode(',', $str, 2));
+    // echo "<br>";
+    
+    // // negative limit
+    // print_r(explode(',', $str, -1));
+    // echo "<br>";
+    
+    // echo hex2bin(bin2hex($str));
+    
+    // $str = '<a href="https://www.w3schools.com">Go to w3schools.com</a>';
+    // echo "$str <br>";
+    // echo htmlentities($str);
+    // echo "<br>";
+    // echo html_entity_decode(htmlentities($str));
+    // echo "<br>";
+    // echo htmlspecialchars($str);
+    // echo "<br>";
+    // echo htmlspecialchars_decode(htmlspecialchars($str));
+    
+    // echo lcfirst($str);
+    // echo "<br>";
+    // echo ucfirst($str);
+    // echo "<br>";
+    // echo ucwords($str);
+    // echo "<br>";
+    // echo strtoupper($str);
+    // echo "<br>";
+    // echo strtolower($str);
+    // echo "<br>";
+
+    // parse_str("id=1&name=Sajal", $a);
+    // foreach ($a as $key => $value) {
+    //     echo $key, ": ", $value, "<br>";
+    // }
+
+    // echo str_ireplace("WORLD","Peter","world Hello world!", $count);
+    // echo $count;
+    
+    // echo str_pad($str, 20, "..", STR_PAD_RIGHT);
+    // echo str_pad($str, 20, "..", STR_PAD_BOTH);
+    // echo str_pad($str, 20, "..", STR_PAD_LEFT);
+
+    // echo str_repeat($str, 5);
+    // print_r(str_split($str, 5));
+
+    // echo strip_tags("Hello <b><i>world!</i></b>","<b>");
+
+    
+
+    ?>
+</body>
+
+</html>
