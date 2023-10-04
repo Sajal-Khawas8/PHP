d3de2827c18374c1722e3b7be054e3e666b7cb3d
SAJAL KHAWAS <khawassajal756@gmail.com>
2023-10-04 14:43:57 +0530

    Changed the logic

--- Task-2-LeastPositiveInteger.php
+++ Task-2-LeastPositiveInteger.php
@@ -1,23 +1,37 @@
 <?php
-$arr = array(-1, -3, 1, 2, 3);
+// $arr = array(-1, -3, 1, 2, 3);
+// sort($arr);
+
+// $ans = array();
+// function leastPositive($v1, $v2)
+// {
+//     global $ans;
+//     if ($v2 - $v1 > 1 && $v1 > 0) {
+//         array_push($ans, $v1 + 1);
+//     }
+//     return $v2;
+// }
+// array_reduce($arr, "leastPositive");
+// if ((!in_array(1, $arr)) || ($arr[count($arr) - 1] <= 0)) {
+//     echo 1;
+// } elseif (count($ans)) {
+//     echo current($ans);
+// } else {
+//     echo $arr[count($arr) - 1] + 1;
+// }
+// // print_r($ans);
+
+
+$arr = array(-1, -2, -3, 1, 3);
 sort($arr);
-$ans = array();
-function leastPositive($v1, $v2)
+$allInts = range(1, end($arr) + 1);
+$missingNumbers = array_diff($allInts, $arr);
+function positiveInt($v)
 {
-    global $ans;
-    if ($v2 - $v1 > 1 && $v1 > 0) {
-        array_push($ans, $v1 + 1);
-    }
-    return $v2;
+    return $v > 0;
 }
-array_reduce($arr, "leastPositive");
-if ((!in_array(1, $arr)) || ($arr[count($arr) - 1] <= 0)) {
-    echo 1;
-} elseif (count($ans)) {
-    echo current($ans);
-} else {
-    echo $arr[count($arr) - 1] + 1;
-}
-// print_r($ans);
+
+$ans = array_filter($missingNumbers, "positiveInt");
+echo current($ans);
 
 ?>
