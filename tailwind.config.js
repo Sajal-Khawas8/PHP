a615f9d9dbeae6643b30acda9513a5695fcf84c7
SAJAL KHAWAS <khawassajal756@gmail.com>
2023-10-13 16:52:40 +0530

    Templates completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,php,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'openSans': ['"Open Sans"', 'sans-serif', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
